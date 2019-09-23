0d941541fc84e5cc16bc1ff2d1c7f2ba1aa60cc1
zhanghao <zlyheut@163.com>
2019-09-23 20:15:21 +0800

    不修改版本号并且修改export内容

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const showMsg = () => {
   console.log(' this is pandahao first npm package to test ')
 }
 const add = (a, b) => a + b + 3
-modelu.exports =  {
+module.exports = {
   showMsg,
   add
 }
