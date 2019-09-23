8b8d5c0e6a4a4dae07ef01c4fac54703c5f851d0
zhanghao <zlyheut@163.com>
2019-09-23 19:13:26 +0800

    修改版本号和add +2 +1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const showMsg = () => {
   console.log(' this is pandahao first npm package to test ')
 }
-const add = (a, b) => a + b + 2
+const add = (a, b) => a + b + 2 + 1
 export {
   showMsg,
   add
